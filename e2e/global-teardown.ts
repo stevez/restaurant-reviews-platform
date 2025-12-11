a6956fcbaba293b115a5d283ff5cf1395ef70b2e
Steve Zhang <huigang.zhang@gmail.com>
2025-12-10 23:14:13 -0500

    support nextcov 0.3.0 (#20)

--- e2e/global-teardown.ts
+++ e2e/global-teardown.ts
@@ -9,7 +9,7 @@ import type { FullConfig } from '@playwright/test'
 import { finalizeCoverage } from 'nextcov/playwright'
 import { loadNextcovConfig } from 'nextcov'
 
-export default async function globalTeardown(_config: FullConfig) {
+export default async function globalTeardown() {
   // Load config from playwright.config.ts (now in root)
   const config = await loadNextcovConfig(path.join(process.cwd(), 'playwright.config.ts'))
 
